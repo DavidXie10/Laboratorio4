779ab4a1d85e44d59508d040bc1f449a2d2a0e66
DavidXie10 <davidxieli@gmail.com>
2021-09-04 21:34:48 -0600

    Laboratorio 4.1 terminado

---
+++ Laboratorio4/Laboratorio4/Controllers/PlanetasController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Laboratorio4.Handlers;
+using Laboratorio4.Models;
+
+namespace Laboratorio4.Controllers
+{
+    public class PlanetasController : Controller
+    {
+        public ActionResult listadoDePlanetas()
+        {
+            PlanetasHandler accesoDatos = new PlanetasHandler();
+            ViewBag.planetas = accesoDatos.obtenerTodoslosPlanetas();
+            return View();
+        }
+    }
+}
