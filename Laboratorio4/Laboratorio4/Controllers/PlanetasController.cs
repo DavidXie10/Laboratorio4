efe6b8e145bc33a358fb697548fe71be51f7d0aa
DavidXie10 <davidxieli@gmail.com>
2021-09-05 14:40:15 -0600

    Crear planetas nuevos: métodos de controlador

--- Laboratorio4/Laboratorio4/Controllers/PlanetasController.cs
+++ Laboratorio4/Laboratorio4/Controllers/PlanetasController.cs
@@ -16,5 +16,36 @@ namespace Laboratorio4.Controllers
             ViewBag.planetas = accesoDatos.obtenerTodoslosPlanetas();
             return View();
         }
+
+        // método para enlazar la vista del formulario
+        public ActionResult crearPlaneta()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult crearPlaneta(PlanetaModel planeta)
+        {
+            ViewBag.ExitoAlCrear = false;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    PlanetasHandler accesoDatos = new PlanetasHandler();
+                    ViewBag.ExitoAlCrear = accesoDatos.crearPlaneta(planeta);
+                    if(ViewBag.ExitoAlCrear)
+                    {
+                        ViewBag.Message = "El planeta" + " " + planeta.nombre + " fue creado con éxito :)";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "Algo salió mal y no fue posible crear el planeta :(";
+                return View(); // si falla se regresa a la vista original pero sin el mensaje
+            }
+        }
     }
 }
