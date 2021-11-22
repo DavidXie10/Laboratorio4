02dc65c6e229d35c532c31d69285bfbd1f7a76b0
DavidXie10 <davidxieli@gmail.com>
2021-11-22 11:30:34 -0600

    Unit test implemented before excercises

--- Laboratorio4/UnitTestLab10/Controllers/PlanetasControllerTest.cs
+++ Laboratorio4/UnitTestLab10/Controllers/PlanetasControllerTest.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Laboratorio4.Controllers;
 using System.Web.Mvc;
+using System.Web;
+using Laboratorio4.Models;
 
 namespace UnitTestLab10.Controllers {
     [TestClass]
@@ -26,6 +28,74 @@ namespace UnitTestLab10.Controllers {
 
             // Assert
             Assert.AreEqual("crearPlaneta", vista.ViewName);
+        }
+
+        [TestMethod]
+        public void EditarPlanetaIdValidoVistaNoNula() {
+            // Arrange
+            int id = 1;
+            PlanetasController planetasController = new PlanetasController();
+
+            // Act
+            ViewResult vista = planetasController.editarPlaneta(id) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(vista);
+        }
+
+        [TestMethod]
+        public void EditarPlanetaValidoModeloRetornadoNoEsNulo() {
+            // Arrange
+            int id = 1;
+            PlanetasController planetasController = new PlanetasController();
+
+            // Act
+            ViewResult vista = planetasController.editarPlaneta(id) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(vista.Model);
+        }
+
+        [TestMethod]
+        public void EditarPlanetaConIdNoExistenteRedirectToLP() {
+            // Arrange
+            int idInvalido = -1;
+            PlanetasController planetasController = new PlanetasController();
+
+            // Act
+            RedirectToRouteResult vista = planetasController.editarPlaneta(idInvalido) as RedirectToRouteResult;
+
+            // Assert
+            Assert.AreEqual("listadoDePlanetas", vista.RouteValues["action"]);
+        }
+
+        [TestMethod]
+        public void EditarPlanetaElModeloEnviadoEsCorrecto() {
+            // Arrange
+            int id = 1;
+            PlanetasController planetasController = new PlanetasController();
+
+            // Act
+            ViewResult vista = planetasController.editarPlaneta(id) as ViewResult;
+            PlanetaModel planeta = vista.Model as PlanetaModel;
+
+            // Assert
+            Assert.IsNotNull(planeta);
+            Assert.AreEqual(6, planeta.numeroAnillos);
+            Assert.AreEqual("Tierra", planeta.nombre);
+        }
+
+        [TestMethod]
+        public void ListadoDePlanetasCantidadDePlanetasEsCorrecta() {
+            // Arrange
+            int numeroPlanetas = 10;
+            PlanetasController planetasController = new PlanetasController();
+
+            // Act
+            ViewResult vista = planetasController.listadoDePlanetas() as ViewResult;
+
+            // Assert
+            Assert.AreEqual(numeroPlanetas, vista.ViewBag.planetas.Count);
         }
     }
 }
